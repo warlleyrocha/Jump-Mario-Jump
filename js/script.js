8c811d05dad102ab19c04588e81138a9f1d0b8c8
Warlley Rocha <warlleyrocha@icloud.com>
2025-01-08 13:15:50 -0300

    feature: adicionar aviso de 'game-over' e opção de restart, relacionar fontes no html, criar css da tela de restart, ajustar altura para 100% do gameboard, criar função no js para exibir a tela

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
+const restartScreen = document.querySelector(".restart-screen");
 
 const jump = () => {
   mario.classList.add("jump");
@@ -29,7 +30,21 @@ const loop = setInterval(() => {
     mario.style.marginLeft = "50px";
 
     clearInterval(loop);
+    restartScreen.style.display = "flex";
   }
 }, 10);
 
 document.addEventListener("keydown", jump);
+
+function restartGame() {
+  restartScreen.style.display = "none";
+  pipe.style.animation = "";
+  mario.style.animation = "";
+  mario.src = "/assets/images/mario.gif";
+  mario.style.width = "150px";
+  mario.style.marginLeft = "0px";
+
+  setTimeout(() => {
+    location.reload();
+  }, 100);
+}
